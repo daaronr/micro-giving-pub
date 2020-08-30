7d00c4b08b5657b10aaf0d20326774fa731d9fb9
David Reinstein <davidr@profusion.com>
2020-08-30 17:51:29 +0100

    add material on utility functions, exercises

--- beem101_bd_online/code/project_setup.R
+++ beem101_bd_online/code/project_setup.R
@@ -18,58 +18,33 @@ where <- pryr::where
 
 #### Sourcing R scripts and HTML formatting ####
 
+#Function to try and download
+try_download <- function(url, path) {
+  new_path <- gsub("[.]", "X.", path)
+  tryCatch({
+    download.file(url = url,
+                  destfile = new_path)
+  }, error = function(e) {
+    print("You are not online, so we can't download")
+  })
+  tryCatch(
+    file.rename(new_path, path
+    )
+  )
+}    
+
 #Downloading html Formatting
-tryCatch(
-  {
-    download.file(url = "https://raw.githubusercontent.com/daaronr/dr-rstuff/master/bookdown_template/support/header.html", destfile = here("support", "headerX.html"))
-    download.file(url = "https://raw.githubusercontent.com/daaronr/dr-rstuff/master/bookdown_template/support/tufte_plus.css", destfile = here("support", "tufte_plusX.css"))
-  },  error = function(e) {
-    print("you are not online, so we can't download")
-  }
-)
+try_download("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/bookdown_template/support/header.html", here("support", "header.html"))
 
-tryCatch(
-  file.rename(here("support", "headerX.html"), here("support", "header.html"))
-)
+try_download("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/bookdown_template/support/tufte_plus.css", here("support", "tufte_plusX.css"))
 
-tryCatch(
-  file.rename(here("support", "tufte_plusX.css"), here("support", "tufte_plus.css"))
-)
-tryCatch(
-  file.rename(here("support", "reinstein_bibtex_dropboxX.bib"), here("support", "reinstein_bibtex_dropbox.bib"))
-)
+try_download("https://www.dropbox.com/s/3i8bjrgo8u08v5w/reinstein_bibtex.bib?raw=1", here("support", "reinstein_bibtex_dropbox.bib"))
 
-#Downloading Bib files
-tryCatch(
-  {
-    download.file(url = "https://www.dropbox.com/s/3i8bjrgo8u08v5w/reinstein_bibtex.bib?raw=1", destfile = here("support", "reinstein_bibtex_dropboxX.bib"))
-
-
-    file.rename(here("support", "reinstein_bibtex_dropboxX.bib"), here("support", "reinstein_bibtex_dropbox.bib"))
-  }
-)
 
 #Source R functions and baseoptions
 
-tryCatch(
-  {
-    download.file(url = "https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/functions.R", destfile = here("code", "functionsX.R"))
+try_download("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/functions.R", here("code", "functions.R"))
+try_download("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/baseoptions.R", here("code", "baseoptions.R"))
 
-    download.file(url = "https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/baseoptions.R", destfile = here("code", "baseoptionsX.R"))
-  },  error = function(e) {
-    print("you are not online, so we source locally instead; hope you've updated")
-    source(here("code", "functions.R")) # functions grabbed from web and created by us for analysis/output
-    source(here("code", "baseoptions.R")) # Basic options used across files and shortcut functions, e.g., 'pp()' for print
-  }
-)
-
-#Note - workaround naming and code because otherwise a failed download seems to delete the destination file -- how to fix that? Renaming files?
-
-file.rename(here::here("code", "functionsX.R"), here::here("code", "functions.R"))
-file.rename(here::here("code", "baseoptionsX.R"), here::here("code", "baseoptions.R"))
-
-source(here("code", "functions.R"))
-source(here("code", "baseoptions.R"))
-
-# Basic options used across files and shortcut functions, e.g., 'pp()' for print
-# functions grabbed from web and created by us for analysis/output
+source(here("code", "functions.R")) # functions grabbed from web and created by us for analysis/output
+source(here("code", "baseoptions.R")) # Basic options used across files and shortcut functions, e.g., 'pp()' for print
